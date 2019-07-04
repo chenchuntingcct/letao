ad3f7fc1a0c9f16ea00417220c051c11efcd53bf
chencct <1120580801@qq.com>
2019-07-05 02:01:18 +0800

    用户管理完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,13 +1,76 @@
-
+$(function(){
+    //
+    var currentPage=1;
+    var pageSize = 5;
+    var currentId; 
+    var currentState
+    
+        getUserInfo();
+  
   //获取表格信息
   function getUserInfo () {
     $.ajax({
       type: "get",
       url:"/user/queryUser",
+      data:{
+        page: currentPage,
+        pageSize: pageSize
+      },
       dataType:"json",
       success:function (data) {
         console.log(data)
+        //template(模板ID, 对象)
+        var htmlStr = template('tpl', data)
+        $("tbody").html(htmlStr)
+        $("#paginator").bootstrapPaginator({
+          //配置bootrap版本
+          bootstrapMajorVersion: 3,
+          //指定总页数
+          totalPages: Math.ceil(data.total / data. size),
+          //当前页
+          currentPage:data.page,
+          onPageClicked: function(a,b,c,page) {
+            console.log(page)
+            //通过page获取点击的页码
+            currentPage= page;
+            getUserInfo();
+          }
+        })
       }
     })
   }
+  //禁用 启用
+  $("tbody").off('click').on('click','.btn' ,function () {
+    //显示模态框
+    $("#userStateModel").modal("show");
+      currentId = $(this).parent().data("id");
+      currentState = $(this).hasClass(".btn-danger")?1:0
+      console.log(currentId)
+      changeUserState ();
+  })
+  
+  //切换用户状态
+  function changeUserState () {
+    $("#submitBtn").click(function (){
+      console.log("我切换状态了吗")
+      $.ajax({
+        type: "POST",
+        url:"/user/updateUser",
+        data:{
+          id:currentId,
+          isDelete:currentState
+        },
+        dataType:"json",
+        success: function (data) {
+          if(data.success){
+            $("#userStateModel").modal("hide");
+            getUserInfo();
+          }
+        }
+      })
+    })
+  }
+})
+  
+  
   
